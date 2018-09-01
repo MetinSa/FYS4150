9bbd9daf1012a7deff286808ca3dd9637a0ec8cf
Metin San <metinisan@gmail.com>
2018-09-01 13:34:17 +0200

    Making a general agorityhm for 3 diag elements a,b and c

---
+++ Project_1/general_algo.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+#include <time.h>
+#include <fstream>
+#include "functions.h"
+
+using namespace std;
+
+// defining the outputfile as a global variable
+ofstream ofile;
+
+
+// // computing the input function f.
+// inline double func(double x){ return 100.0*exp(-10*x); 
+// }
+
+// // computing the exact solution to the problem
+// inline double analytic_sol(double x){ return 1.0 - (1 - exp(-10))*x - (exp(-10 * x));
+// }
+
+// Main program
+
+int main(int argc, char *argv[]){
+
+	//declaring the exponent of 10
+	// int expo; 
+
+	// if ( argc <= 1) {
+	// 	cout << "Bad Usage: Please include the power of 10^n." << endl;
+	// 	exit(1);
+	// }
+	// 	else{
+	// 	expo = atoi(argv[1]);
+
+	// }
+
+	return 0;
+
+}

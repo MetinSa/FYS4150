6f6a6f66af4f55569607b8f4feb85c98314af3a7
maraspr <markus@asprusten.com>
2018-09-04 22:21:08 +0200

    Tried and failed to solve with LU

--- Project_1/1e/1e_armadillo.cpp
+++ Project_1/1e/1e_armadillo.cpp
@@ -20,17 +20,24 @@ inline double exact_solution(double x) {return 1.-(1-exp(-10))*x-exp(-10*x);}
 
 int main(int argc, char** argv)
   { 	
-    int expo; int n;
+    int n;
 
     // Reading in values from the command line
-    if ( argc <= 2)
+    if ( argc < 2)
     {
   		cout << "Bad Usage: Please include the power of 10^n, and the content of at least the first diagonal vector. At least 2 inputs." << endl;
   		cout << "Diagonal values is first middel, then first upper, then first lower, then second upper, then second lower etc." << endl;
       exit(1);
   	}
-    expo = atoi(argv[1]);
-    n = pow(10.0, expo);
+    
+    if (string(argv[1]) == "test")
+    {
+      n = 4;
+    }
+    else
+    {
+      n = pow(10.0, atoi(argv[1]));
+    }
 
     if (argc - 2 > n*2 -1)
     {
@@ -58,6 +65,7 @@ int main(int argc, char** argv)
     mat L, U;    // Assign armadillo matrices, lower and upper triangular for LU decomposition
 
     mat A(n, n); // Assign matrix to be decomposed
+    A.fill(0);
 
     for (int i = 0; i<n; i++)
     {
@@ -82,6 +90,15 @@ int main(int argc, char** argv)
     double x_0 = 0;    double x_n = 1;   // Boundary conditions
     double h = (x_n - x_0)/n;            // Step size
 
+    vec b(n); vec z(n); vec x(n); vec c(n); vec exac(n);
+    double sum;
+
+    for (int i = 0; i < n; i++)
+    {
+      x(i) = x_0 + i*h;
+      b(i) = func(x(i))*h*h;
+      exac(i) = exact_solution(x(i));
+    }
 
     //starting the clock
   	clock_t start, finish;
@@ -90,9 +107,26 @@ int main(int argc, char** argv)
     lu(L, U, A);   // LU-decomposition
 
     cout << "Upper triangular:\n" << U << endl << endl;
+    cout << "Lower triangular:\n" << L << endl;
 
-    cout << "Lower triangular:\n" << L;
+    // Solving Ac = b => LUc = b => Lz = b => Uc = z
+    // Forward substitution, finding Z
 
+    z(0) = b(0);
+    for (int i = 1; i < n; i++)
+    {
+      z(i) = b(i)-b(i-1)*L(i,i-1);
+    }
+
+    c(n-1) = z(n-1)/U(n-1, n-1);
+    // Backward substitution, finding c
+    for (int i = n-2; i == 0; i--)
+    {
+      c(i) = z(i)/U(i,i) - z(i+1)*U(i,i+1);
+    }
+
+    // FAILED HORRIBLY
+    cout << exac << endl << c;
     // Stopping the clock
   	finish = clock();
   	double timeused = (double) (finish - start)/(CLOCKS_PER_SEC);

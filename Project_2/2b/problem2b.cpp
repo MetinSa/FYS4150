272d799bd5608308bb38adbbd52dc879e38ad9c6
Metin San <metinisan@gmail.com>
2018-09-12 13:02:48 +0200

    included analytic calculation of eigenvalues

--- Project_2/2b/problem2b.cpp
+++ Project_2/2b/problem2b.cpp
@@ -39,6 +39,27 @@ arma::mat constructA(int n, double rho_0, double rho_n){
 
 }
 
+
+arma::vec Analytic_Eigenvalues(int n){
+	//function that computes the analytic eigenvalues of a toeplitz tridiagonal matrix
+
+	//making empty lambda
+	arma::vec lambda = arma::vec(n);
+	lambda.zeros();
+
+	//defining stepsize
+	double h = (rho_n - rho_0)/n;
+
+	//using analytic function to find eigen values for the matrix A
+	for (int i = 0; i < n; i++){
+		lambda(i) = (2/h*h) - 2*(1/(h*h))*cos((i*M_PI)/(n+1));
+	}
+
+	return lambda;
+}
+
+
+
 int main(int argc, char *argv[]){
 
 	//reading in number of gridpoints
@@ -47,9 +68,10 @@ int main(int argc, char *argv[]){
 	double rho_n = atof(argv[3]);
 
 	arma::mat A(n,n);
+	arma::mat lambda(n,n);
 
 	A = constructA(n, rho_0, rho_n);
-	A.print("A: ");
-
+	lambda = Analytic_Eigenvalues(n, rho_0, rho_n);
+	lambda.print("lambda: ");
 	return 0;
 }

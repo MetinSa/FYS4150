bc1dd5917fdd0becbe436f33f5099ef939826e2f
maraspr <markus@asprusten.com>
2018-10-18 17:19:36 +0200

    Renamed folders correctly.

--- Project_3/SolarSystem/main.cpp
+++ Project_3/SolarSystem/main.cpp
@@ -8,8 +8,8 @@ int main(int argc, char *argv[]){
 	//SolarSystem sol("SunEarthJupiterSystem.txt");
 	SolarSystem sol("planets.txt");
 	Solver solve(sol);
-	// solve.velocityVerlet(0.001, 10);
-	solve.forwardEuler(0.001, 10);
+	solve.velocityVerlet(0.01, 5);
+	// solve.forwardEuler(0.01, 5);
 
 	return 0;
 

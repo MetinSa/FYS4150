f4a68b787505fe957245c6c8b4c075442411f9e9
Markus Leira Asprusten <markus@asprusten.com>
2018-10-19 13:37:45 +0200

    Made command line arguments optional.

--- Project_3/SolarSystem/main.cpp
+++ Project_3/SolarSystem/main.cpp
@@ -4,11 +4,27 @@
 int main(int argc, char *argv[]){
 
 	double years, dt;
+	std::string systemname;
 
 	// Commandline arguments
-	std::string systemname = argv[1];
-	years = atof(argv[2]);
-	dt = atof(argv[3]);
+	if (argc > 1)
+	{
+		systemname = argv[1];
+		years = 10;
+		dt = 0.01;
+	}
+	else if (argc > 3)
+	{
+		systemname = argv[1];
+		years = atof(argv[2]);
+		dt = atof(argv[3]);
+	}
+	else
+	{
+		systemname = "planets.txt";
+		years = 10;
+		dt = 0.01;
+	}
 
 	// Setting up the system
 	SolarSystem planetary_system(systemname);

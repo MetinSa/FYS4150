901e04fcedc77a746b3f4827f3c6e7627b54195b
mjauren <marenr@live.no>
2018-10-24 11:40:05 +0200

    added relative energy plot

--- Project_3/SolarSystem/plottertools/energyplotter.py
+++ Project_3/SolarSystem/plottertools/energyplotter.py
@@ -22,25 +22,32 @@ else:
 
 
 Ek, Ep, momentum = (np.loadtxt(filename, unpack=True))
+E_tot = Ek + Ep
 
 years = np.linspace(0,10,len(Ek))
 
-plt.figure()
-plt.plot(years, Ek, color='blue', label='Total kinetic energy')
-plt.plot(years, Ep, color='magenta', label='Total potential energy')
-plt.plot(years, Ek+Ep, color='teal', label=r'Total energy ($E_k + E_p$)')
+# plt.figure()
+plt.plot(years, Ek, color='blue', label='Kinetic energy')
+plt.plot(years, Ep, color='magenta', label='Potential energy')
+plt.plot(years, E_tot, color='teal', label=r'Total energy ($E_k + E_p$)')
 plt.grid()
 plt.xlabel('Time [years]')
 plt.ylabel(r'Energy [$M_{sun} (AU/yr)^2$]')
 plt.legend()
-#tikz_save("Figures/figure.tex", figureheight='\\figureheight', figurewidth='\\figurewidth')
-
+tikz_save("Figures/energy_verlet.tex", figureheight='\\figureheight', figurewidth='\\figurewidth')
 
 plt.figure()
-plt.plot(years, momentum, color='blue')
+plt.plot(years, abs(E_tot-E_tot[0]), color='teal')#, label=r'Total energy ($E_k + E_p$)')
 plt.grid()
 plt.xlabel('Time [years]')
-plt.ylabel(r'Total angular momentum per mass [$AU^2/yr$]')
-plt.legend()
-#tikz_save("Figures/figure.tex", figureheight='\\figureheight', figurewidth='\\figurewidth')
+plt.ylabel(r'|Total relative energy| [$M_{sun} (AU/yr)^2$]')
+tikz_save("Figures/relenergy_verlet.tex", figureheight='\\figureheight', figurewidth='\\figurewidth')
 plt.show()
+
+# plt.figure()
+# plt.plot(years, abs(momentum-momentum[0]), color='blue')
+# plt.grid()
+# plt.xlabel('Time [years]')
+# plt.ylabel(r'Total angular momentum per mass [$AU^2/yr$]')
+# #tikz_save("Figures/figure.tex", figureheight='\\figureheight', figurewidth='\\figurewidth')
+# plt.show()

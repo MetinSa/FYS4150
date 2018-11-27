41861ba31d13e8cde69c01decd4634b74459239d
Markus Leira Asprusten <markus@asprusten.com>
2018-11-27 15:45:18 +0100

    Added test rule to makefile, create testfiles with extension .cpp in test-directory, and run all with 'make test'. Parallellize with 'make -j test'.

---
+++ Project_5/test/test1.cpp
@@ -0,0 +1,18 @@
+#include "functions.h"
+
+int main(int argc, char *argv[])
+{
+	bool test = true;
+
+	// Run test, set test = false if test failed
+
+	if (test)
+	{
+		return 0;
+	}
+	else
+	{
+		//std::cout << "Error message" << std::endl;
+		return 1;
+	}
+}
